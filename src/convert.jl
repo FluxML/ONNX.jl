466aa46c7455d16e8cbe37299fb97bc3034e8ca9
ayush1999 <ayush.shridhar1999@gmail.com>
2018-02-13 22:54:23 +0530

    Docstrings fixed

--- src/convert.jl
+++ src/convert.jl
@@ -1,29 +1,29 @@
 using ProtoBuf
 include("onnx_pb.jl")
 
-function convert_model(model::Any)
-    """
+"""
     Convert a data type to the corresponding dictionary.
-    """
+"""
+function convert_model(model::Any)
     dict = Dict(f=>get_field(model, f) for f in fieldnames(model))
     return dict
 end
 
-function convert_model(model::AttributeProto)
-    """
+"""
     Retrieve only the useful information from a AttributeProto
     object  into a Dict format.
-    """
+"""
+function convert_model(model::AttributeProto)
     attributes = [:name, :_type, :f, :i, :ints]
     dict = Dict(f=>get_field(model, f) for f in attributes)
     return dict
 end
 
-function get_array(model::TensorProto)
-    """
+"""
     Get _the_ array from a TensorProto object.
     Since :raw_data is the only attribute storing valid array data, we\'ll
     retrieve it.
-    """
+"""
+function get_array(model::TensorProto)
     return model.raw_data
 end
