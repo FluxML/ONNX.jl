b8e2c7574c1f81eab569979c969d4d26b6fae1dd
ayush1999 <ayush.shridhar1999@gmail.com>
2018-04-03 23:39:07 +0530

    Added signatures, removing Conv dependency from Flux

--- src/ONNX.jl
+++ src/ONNX.jl
@@ -6,5 +6,12 @@ include("onnx_pb.jl")
 include("convert.jl")
 include("new_types.jl")
 include("graph/graph.jl")
+include("signatures.jl")
+
+export maxpool2d, relu, softmax
+export Conv, @require, Chain, Dense, RNN, LSTM, GRU,
+        Dropout, LayerNorm, BatchNorm,
+        SGD, ADAM, Momentum, Nesterov, AMSGrad,
+        param, params, mapleaves, cpu, gpu
 
 end # module
