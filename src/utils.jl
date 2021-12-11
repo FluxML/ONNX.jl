fc43a6bc6098b84fd15b5fb92052ec19fd279001
Andrei Zhabinski <faithlessfriend@gmail.com>
2021-12-12 01:14:49 +0300

    Use more concise syntax for rename_keys()
    Co-authored-by: Brian Chen <ToucheSir@users.noreply.github.com>

--- src/utils.jl
+++ src/utils.jl
@@ -3,11 +3,4 @@
 
 Create a copy of `d`, replacing its keys according to mapping `subs`
 """
-function rename_keys(dct::Dict, subs::Dict)
-    new = empty(dct)
-    for (key, val) in dct
-        new_key = get(subs, key, key)
-        new[new_key] = val
-    end
-    return new
-end
+rename_keys(dct::Dict, subs::Dict) = Dict(get(subs, k, k) => v for (k, v) in pairs(dct))
