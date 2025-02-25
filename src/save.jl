7c75e43631074db762565172fe9b5462f5967455
Duncan Starkenburg <duncan@starkenburg.com>
2025-02-25 13:41:18 -0500

    Adding support for ConstantOfShape

--- src/save.jl
+++ src/save.jl
@@ -171,6 +171,11 @@ function save_node!(g::GraphProto, ::OpConfig{:ONNX, typeof(neg)}, op::Umlaut.Ca
     push!(g.node, nd)
 end
 
+function save_node!(g::GraphProto, ::OpConfig{:ONNX, typeof(makeshape)}, op::Umlaut.Call)
+    nd = NodeProto("ConstantOfShape", op)
+    push!(g.node, nd)
+end
+
 function save_node!(g::GraphProto, ::OpConfig{:ONNX, typeof(*)}, op::Umlaut.Call)
     nd = NodeProto(
         input=[onnx_name(v) for v in reverse(op.args)],
